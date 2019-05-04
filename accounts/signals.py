927edad14ac84007620242193ae78ff772472af9
claduva <alphdraftleague@gmail.com>
2019-05-04 08:45:14 -0600

    update

--- accounts/signals.py
+++ accounts/signals.py
@@ -2,7 +2,7 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 
-from .models import profile
+from .models import *
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender,instance,created,**kwargs):
