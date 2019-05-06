3526200a0671aac878a13d597fc4a0202a297f69
claduva <alphdraftleague@gmail.com>
2019-05-06 11:45:30 -0600

    Update

--- individualleague/views.py
+++ individualleague/views.py
@@ -43,63 +43,62 @@ def team_page(request,league_name,team_abbreviation):
     return render(request, 'teampage.html',context)
 
 def league_draft(request,league_name):
-    draftactive=True
     try:
         league_=league.objects.get(name=league_name)
+        league_teams=coachdata.objects.all().filter(league_name=league_).order_by('teamname')
     except:
         messages.error(request,'League does not exist!',extra_tags='danger')
         return redirect('league_list')
-    league_teams=coachdata.objects.all().filter(league_name=league_).order_by('teamname')
     try:
         season=seasonsetting.objects.get(league=league_)
-        try:
-            draftlist=draft.objects.all().filter(season=season).order_by('id')
-            try:    
-                currentpick=draftlist.filter(pokemon__isnull=True).first()
-                draftersroster=roster.objects.all().filter(season=season,team=currentpick.team)
-                pointsused=0
-                for item in draftersroster:
-                    tier=pokemon_tier.objects.filter(league=league_,pokemon=item.pokemon).first()
-                    if tier != None:    
-                        pointsused+=tier.tier.tierpoints
-                budget=season.draftbudget
-                availaplepoints=budget-pointsused
-                if request.method == "POST":
-                    try:
-                        draftpick=all_pokemon.objects.get(pokemon=request.POST['draftpick'])
-                    except:
-                        messages.error(request,f'{request.POST["draftpick"]} is not a pokemon!',extra_tags='danger')
-                        return redirect('league_draft',league_name=league_name)
-                    searchroster=roster.objects.filter(season=season,pokemon=draftpick).first()
-                    if searchroster!=None:
-                        messages.error(request,f'{draftpick.pokemon } has already been drafted!',extra_tags='danger')
-                        return redirect('league_draft',league_name=league_name)
-                    currentpick.pokemon=draftpick
-                    rosterspot=roster.objects.all().order_by('id').filter(season=season,team=currentpick.team,pokemon__isnull=True).first()
-                    rosterspot.pokemon=draftpick
-                    currentpick.save()
-                    rosterspot.save()
-                    messages.success(request,'Your draft pick has been saved!')
-                    return redirect('league_draft',league_name=league_name)
-                availablepokemon=all_pokemon.objects.all().order_by('pokemon')
-                for item in availablepokemon:
-                    tiering=pokemon_tier.objects.all().filter(league=league_,pokemon=item).first()
-                    if tiering.tier.tiername=="Banned":
-                        availablepokemon=availablepokemon.all().exclude(pokemon=item)
-                    else:
-                        rosteritem=roster.objects.filter(season=season,pokemon=item).first()
-                        if rosteritem != None:
-                            availablepokemon=availablepokemon.all().exclude(pokemon=item)
-            except:
-                availablepokemon=availablepokemon.none()
-                draftactive=False
-                return redirect('league_draft',league_name=league_name)
-        except:
-            messages.error(request,'Draft does not exist!',extra_tags='danger')
-            return redirect('league_detail',league_name=league_name)
     except:
         messages.error(request,'Season does not exist!',extra_tags='danger')
         return redirect('league_detail',league_name=league_name)
+    try:
+        draftlist=draft.objects.all().filter(season=season).order_by('id') 
+    except:
+        messages.error(request,'Draft does not exist!',extra_tags='danger')
+        return redirect('league_detail',league_name=league_name)
+    currentpick=draftlist.filter(pokemon__isnull=True).first()
+    if currentpick==None:
+        draftactive=False
+        availablepoints=0
+    else:
+        draftactive=True
+        draftersroster=roster.objects.all().filter(season=season,team=currentpick.team)
+        pointsused=0
+        for item in draftersroster:
+            tier=pokemon_tier.objects.filter(league=league_,pokemon=item.pokemon).first()
+            if tier != None:    
+                pointsused+=tier.tier.tierpoints
+        budget=season.draftbudget
+        availablepoints=budget-pointsused
+    if request.method == "POST":
+        try:
+            draftpick=all_pokemon.objects.get(pokemon=request.POST['draftpick'])
+        except:
+            messages.error(request,f'{request.POST["draftpick"]} is not a pokemon!',extra_tags='danger')
+            return redirect('league_draft',league_name=league_name)
+        searchroster=roster.objects.filter(season=season,pokemon=draftpick).first()
+        if searchroster!=None:
+            messages.error(request,f'{draftpick.pokemon } has already been drafted!',extra_tags='danger')
+            return redirect('league_draft',league_name=league_name)
+        currentpick.pokemon=draftpick
+        rosterspot=roster.objects.all().order_by('id').filter(season=season,team=currentpick.team,pokemon__isnull=True).first()
+        rosterspot.pokemon=draftpick
+        currentpick.save()
+        rosterspot.save()
+        messages.success(request,'Your draft pick has been saved!')
+        return redirect('league_draft',league_name=league_name)
+    availablepokemon=all_pokemon.objects.all().order_by('pokemon')
+    for item in availablepokemon:
+        tiering=pokemon_tier.objects.all().filter(league=league_,pokemon=item).first()
+        if tiering.tier.tiername=="Banned":
+            availablepokemon=availablepokemon.all().exclude(pokemon=item)
+        else:
+            rosteritem=roster.objects.filter(season=season,pokemon=item).first()
+            if rosteritem != None:
+                availablepokemon=availablepokemon.all().exclude(pokemon=item)
     context = {
         'league': league_,
         'leaguepage': True,
@@ -110,6 +109,6 @@ def league_draft(request,league_name):
         'currentpick':currentpick,
         'availablepokemon': availablepokemon,
         'draftactive': draftactive,
-        'availablepoints':availaplepoints
+        'availablepoints':availablepoints
     }
     return render(request, 'draft.html',context)
