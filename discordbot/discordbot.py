b5a9831321989f7fed8ad4144b2cbf7d4b342bac
claduva <alphdraftleague@gmail.com>
2019-05-17 15:19:53 -0600

    update

--- discordbot/discordbot.py
+++ discordbot/discordbot.py
@@ -22,10 +22,9 @@ async def on_message(message):
     if message.content.startswith('!website'):
         await message.channel.send('http://pokemondraftleague.online/')
 
-    if message.content.startswith('Kick Sleepy') and message.author.id==270800855677140994:
+    if message.content.startswith('Kick Sleepy') and (message.author.id==270800855677140994 or message.author.id==396785635991486466):
         sleepy=message.guild.get_member(248633879529783296)
-        await sleepy.kick()
+        await sleepy.kick(reason="LOL Kicked by a bot")
         await message.channel.send('Finally. I thought you would never ask.')
-
 
 client.run(bottoken)
