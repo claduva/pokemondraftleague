66000c8bb7a920092e3f35b32522fe764eca3d6e
claduva <alphdraftleague@gmail.com>
2019-06-13 13:08:49 -0600

    update

--- main/views.py
+++ main/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect, JsonRespons
 from django.shortcuts import render
 from django.urls import reverse
 from django.core import serializers
+from django.db.models import Q
 
 import json
 from datetime import datetime
@@ -10,9 +11,20 @@ import time
 
 from accounts.forms import UserRegisterForm
 from .models import *
+from leagues.models import *
 
 # Create your views here.
 def home(request):
+    try:
+        yourleagues=coachdata.objects.all().filter(Q(coach=request.user)|Q(teammate=request.user))
+        if yourleagues.count()>0:
+            context = {
+                "title": "Pokemon Draft League",
+                "yourleagues": yourleagues,
+            }
+            return  render(request,"coachlandingpage.html", context)
+    except:
+        print("error")
     form=form = UserRegisterForm()
     context = {
         "title": "Pokemon Draft League",
