da572ee5572c76b9783fe3633b61f6360d428789
Colton Ladbury <coltonladbury@Coltons-MacBook-Air.local>
2020-01-14 04:47:02 -0700

    update

--- replayanalysis/urls.py
+++ replayanalysis/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [ 
     path("replayanalysis/", views.replay_analysis, name="replay_analysis"),
     path("leagues/<str:league_name>/<str:subleague_name>/uploadreplay/<int:matchid>", views.upload_league_replay, name="upload_league_replay"),
-    path("leagues/<str:league_name>/<str:subleague_name>/uploadreplay/<int:matchid>/manual/", views.upload_league_replay_manual, name="upload_league_replay_manual"),
+    #path("leagues/<str:league_name>/<str:subleague_name>/uploadreplay/<int:matchid>/manual/", views.upload_league_replay_manual, name="upload_league_replay_manual"),
     path("leagues/<str:league_name>/<str:subleague_name>/matchresults/<int:matchid>/", views.league_match_results, name="league_match_results"),
     path("checkanalyzer/", views.check_analyzer, name="check_analyzer"),
     path("checkanalyzer/currentmatch/", views.check_current_match, name="check_current_match"),
