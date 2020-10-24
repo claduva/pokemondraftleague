45da8bed645245814a9d743956a0478bf99fef74
claduva <cladbury@gmail.com>
2020-10-24 13:56:43 -0700

    update

--- api/urls.py
+++ api/urls.py
@@ -3,8 +3,8 @@ from rest_framework import routers
 from api import views
 
 router = routers.DefaultRouter()
-router.register(r'overdue', views.OverdueViewSet,base_name='overdue')
-router.register(r'schedule', views.ScheduleViewSet,base_name='schedule')
+router.register(r'overdue', views.OverdueViewSet,basename='overdue')
+router.register(r'schedule', views.ScheduleViewSet,basename='schedule')
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
