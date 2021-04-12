d48c7552c06a8bcd989a7c964e473ea67d97a6dd
Colton Ladbury <cladbury@gmail.com>
2021-04-12 09:19:02 -0700

    update

--- leagues/urls.py
+++ leagues/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [ 
     path("leagues/", views.league_list, name="league_list"),
-    path("leagues/createleague/", views.create_league, name="create_league"),
+    #path("leagues/createleague/", views.create_league, name="create_league"),
     path("settings/league/", views.leagues_hosted_settings, name="leagues_hosted_settings"),
     path("settings/coaching/", views.leagues_coaching_settings, name="leagues_coaching_settings"),
     path("settings/coaching/<str:league_name>/", views.individual_league_coaching_settings, name="individual_league_coaching_settings"),
@@ -20,7 +20,7 @@ urlpatterns = [
     path("settings/league/<str:league_name>/managecoachs/removecoach/", views.remove_coach, name="removecoach"),
     path("settings/league/<str:league_name>/managecoach/<str:coachofinterest>/", views.manage_coach, name="manage_coach"),
     path("settings/league/<str:league_name>/addteamofcoachs", views.add_team_of_coachs, name="add_team_of_coachs"),
-    path("settings/league/<str:league_name>/archiveseason/", views.archive_season, name="archive_season"),
+    #path("settings/league/<str:league_name>/archiveseason/", views.archive_season, name="archive_season"),
     path("settings/league/<str:league_name>/<str:subleague_name>/managetiers/", views.manage_tiers, name="manage_tiers"),
     path("settings/league/<str:league_name>/<str:subleague_name>/managetiers/tier/<str:tier>/", views.view_tier, name="view_tier"),
     path("settings/league/<str:league_name>/<str:subleague_name>/managetiers/default/", views.default_tiers, name="default_tiers"),
